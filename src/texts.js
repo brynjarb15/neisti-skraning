b38b66f91d62d2ea011d7a286a4195f9d185a113
brynjarb15 <brynjar_b@hotmail.com>
2019-11-25 23:28:57 +0000

    Start of moving all texts into 1 file where you can edit them all at the same place

---
+++ src/texts.js
@@ -0,0 +1,9 @@
+const texts = {
+    topInstructions = "Veljið þær 6-10 smiðjur sem þið hafið mestan áhuga á.  Númerið þær eftir áhugaröð,  þ.e. 1 fyrir þá sem þið hafið mestan áhuga á og svo framvegis. Nauðsynlegt er að velja 6 smiðjur og hámark er 10 smiðjur <br></br> "+
+    "Reynið að velja í bland smiðjur sem taka 1 klst og þær sem taka 2.5 klst. <br></br> " + 
+    "Athugið að lágmarksþátttaka þarf að nást til að smiðja verði starfrækt.",
+    fillOutThisInfo = "Fylla þarf út eftirtaldar upplýsingar"
+
+};
+
+export default texts;
